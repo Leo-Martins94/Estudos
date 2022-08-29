cdf080a4604388f765cf9819bee29183aec21e67
Leo-Martins94 <leomartins2194@gmail.com>
2022-08-29 17:03:01 -0300

    Java inicio de vetores Curso em Video

---
+++ Java/CursoEmVideo/vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor01;
+
+/**
+ *
+ * @author leona
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n [] = {2,62,4,23,6,5,1,26,32};
+        
+       for(int c = 0; c < n.length ; c++){
+           System.out.println(n[c]);
+       }
+    }
+    
+}
