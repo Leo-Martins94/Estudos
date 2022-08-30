40724f658cb05bca3ae882731ad2ca578e1c6ff1
Leo-Martins94 <leomartins2194@gmail.com>
2022-08-30 09:06:06 -0300

    Java aula 02 CFB Cursos

---
+++ Java/CFBCursos/CursoJava.java
@@ -0,0 +1,13 @@
+public class CursoJava {
+    public static void main(String[] args){
+        
+        int n1=10, n2=20,n3=30;
+        int res = n1+n2+n3;
+        String nome = "Leonardo";
+
+        System.out.print("CFB Cursos\n");
+        System.out.println("Curso de Java");
+        System.out.printf("Canal:%s - Ano:%d%n","CFFB Cursos", 2022);
+        System.out.printf("Valor da variavel num:%d%nNome:%s",res,nome);
+    }
+}
