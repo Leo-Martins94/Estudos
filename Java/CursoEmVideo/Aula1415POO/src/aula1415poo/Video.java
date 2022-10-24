fbec982ee0921396135bb4c8e44d89c2ad93581c
Leo-Martins94 <leomartins2194@gmail.com>
2022-10-24 16:25:24 -0300

    Java Final-Aula15-POO CursoEmVideo

--- Java/CursoEmVideo/Aula1415POO/src/aula1415poo/Video.java
+++ Java/CursoEmVideo/Aula1415POO/src/aula1415poo/Video.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package aula1415poo;
 
 /**
@@ -9,5 +5,52 @@ package aula1415poo;
  * @author leona
  */
 public class Video {
-    
+   private String titulo;
+   private int avaliacao;
+   private int views;
+   private int curtidas;
+   private boolean reproduzindo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        int nova;
+        nova = (int) ((this.avaliacao + avaliacao )/this.views);
+        this.avaliacao = avaliacao;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public boolean isReproduzindo() {
+        return reproduzindo;
+    }
+
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
+    }
+   
 }
