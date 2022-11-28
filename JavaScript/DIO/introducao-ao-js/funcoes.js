336a6bfb1eadcd71f8674655153b41da7e6714df
Leo-Martins94 <leomartins2194@gmail.com>
2022-11-28 10:48:13 -0300

    JavaScript Recursos basicos DIO

---
+++ JavaScript/DIO/introducao-ao-js/funcoes.js
@@ -0,0 +1,7 @@
+function soma(a, b){
+    return a + b;
+}
+
+soma(10,10);
+
+
---
+++ JavaScript/DIO/introducao-ao-js/variaveis.js
@@ -0,0 +1,9 @@
+//var x = 0;
+
+/*var x = 0;
+var y = 1;*/
+
+var preco = 2;
+const desconto = 0.2;
+
+preco = preco - desconto;
